45896c22dbf8d7155fe04e593a38cd3ddd703d2a
Cristofer Arias Calizaya <cristoferarias2005@gmail.com>
2026-07-03 13:39:31 -0400

    Fix: Oficina

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Administracion from "./components/Administracion/Administracion.jsx";
 import UnidadAdministrativa from "./components/Unidad Administrativa/Unidad Administrativa.jsx";
+import Oficina from './components/Oficina/Oficina.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Administracion/>
     <UnidadAdministrativa/>
-  </StrictMode>,
+    <Oficina/>
+  </StrictMode>
 )
